c4e421d434e307559c10a39a565360ed7882ed9f
= <=>
2021-09-05 18:27:55 -0500

    Se agrega menu lateral javascript

--- js/main.js
+++ js/main.js
@@ -1,28 +1,6 @@
-const slider = document.querySelector("#slider");
-let sliderSection = document.querySelectorAll(".slider_img");
-let sliderSectionLast = sliderSection[sliderSection.length -1];
+let btn_menu = document.querySelector('.btn_menu');
+let menu = document.querySelector('.nav_var');
 
-const btnLeft = document.querySelector("#btn_left");
-const btnRight = document.querySelector("#btn_right");
-
-slider.insertAdjacentElement('afterbegin', sliderSectionLast);
-
-function moverDerecha() {
-    let sliderSectionFrist = document.querySelectorAll(".slider_img")[0];
-    slider.style.marginLeft = "-200%";
-    slider.style.transition = "all 0.5s";
-    setTimeout(function(){
-        slider.style.transition = "none";
-        slider.insertAdjacentElement('beforeend', sliderSectionFrist);
-        slider.style.marginLeft = "-100%";
-
-    },500);
-}
-
-btnRight.addEventListener('click',function(){
-    moverDerecha();
-});
-
-setInterval(() => {
-    moverDerecha();
-}, 4000);
+btn_menu.addEventListener('click', () => {
+    menu.classList.toggle('show');
+})
