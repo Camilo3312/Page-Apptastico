af680a25e3eb3cf6f335fb77b580207201e6c29a
= <=>
2021-09-05 19:04:01 -0500

    Se agregan estilos a el header

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 let btn_menu = document.querySelector('.btn_menu');
+let header = document.querySelector('.header');
+
 let menu = document.querySelector('.nav_var');
 
 btn_menu.addEventListener('click', () => {
     menu.classList.toggle('show');
 })
+
+window.addEventListener('scroll', () =>{
+    
+});
