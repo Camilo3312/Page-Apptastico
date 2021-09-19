37c2deb8d0ef6bfe8fb42c7804c12c6ed587d21d
= <=>
2021-09-19 01:49:53 -0500

    Se añade estios a el header

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
 let btn_menu = document.querySelector('.btn_menu');
-
 let menu = document.querySelector('.nav_var');
+let header = document.querySelector('.header');
 
 btn_menu.addEventListener('click', () => {
-    menu.classList.toggle('show');
-})
+    menu.classList.toggle('show');  
+});
+
+window.addEventListener('scroll', ()=> {
+    header.classList.toggle('color',window.scrollY > 1000); 
+});
